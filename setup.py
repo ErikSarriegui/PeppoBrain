ffd6ac94e54967d7fe2393c2fcd28cad8c9e48ab
Erik Sarriegui <92121483+ErikSarriegui@users.noreply.github.com>
2025-01-26 15:51:40 +0100

    Commit

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="peppobrain",
     version="0.1.0",
-    packages=["peppobrain"],
+    packages=["peppobrain", "peppobrain_utils"],
     install_requires=["numpy"],
 )
