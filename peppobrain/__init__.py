fef35444a6cfe5adc8262b937ca9f5522af01979
Erik Sarriegui <92121483+ErikSarriegui@users.noreply.github.com>
2025-01-26 15:45:52 +0100

    Resolving import issues

--- peppobrain/__init__.py
+++ peppobrain/__init__.py
@@ -1,2 +1,10 @@
-from . import nn
-from . import utils
+def _import_nn():
+    import peppobrain.nn
+    return peppobrain.nn
+
+def _import_utils():
+    import peppobrain.utils
+    return peppobrain.utils
+
+nn = _import_nn()
+utils = _import_utils()
